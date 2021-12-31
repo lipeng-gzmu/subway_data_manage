19c2a185228fc2f446787b958b02d97a1bb60a1a
lipeng-gzmu <1826427954@qq.com>
2021-12-31 12:46:44 +0800

    增加city注解

--- src/main/java/com/gzmu/lpzyf/bean/City.java
+++ src/main/java/com/gzmu/lpzyf/bean/City.java
@@ -2,7 +2,10 @@ package com.gzmu.lpzyf.bean;
 
 import lombok.*;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class City {
     private Integer id;
     private String nameCn;
