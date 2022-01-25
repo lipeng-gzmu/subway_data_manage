188ae32ec3edb6d01d08c98a64c31d1ab8175f74
zhangyanfang-gzmu <1424270288@qq.com>
2022-01-25 15:58:57 +0800

    张延芳：查询每个城市的地铁线路数量

--- src/main/java/com/gzmu/lpzyf/mapper/MetroLineMapper.java
+++ src/main/java/com/gzmu/lpzyf/mapper/MetroLineMapper.java
@@ -3,7 +3,11 @@ package com.gzmu.lpzyf.mapper;
 import com.gzmu.lpzyf.bean.MetroLine;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 @Repository
 public interface MetroLineMapper {
     void insert(MetroLine metroLine);
+    //查询每个城市的地铁线路数
+    Integer findLineCount(Integer city_id);
 }
