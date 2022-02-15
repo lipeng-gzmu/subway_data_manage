1d3a753a733772044f69cf318c463f8f2821a204
zhangyanfang-gzmu <1424270288@qq.com>
2022-02-15 17:10:20 +0800

    张延芳：添加注释

--- src/main/java/com/gzmu/lpzyf/controller/SkipPagesController.java
+++ src/main/java/com/gzmu/lpzyf/controller/SkipPagesController.java
@@ -3,23 +3,30 @@ package com.gzmu.lpzyf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+* 实现页面跳转
+* */
 @Controller
 public class SkipPagesController {
+    //首页
     @RequestMapping("/index")
     public String skipIndex(){
         return "index";
     }
 
+    //数据采集
     @RequestMapping("/getData")
     public String skipData(){
         return "SpeedOfProgress";
     }
 
+    //地铁网图
     @RequestMapping("/network")
     public String skipNetword(){
         return "network";
     }
 
+    //注册
     @RequestMapping("/to_register")
     public String to_register(){
         return "register";
